ad5047c3a6ee3b7b0b6ab66192cee759ce5c5018
皮哥 <1056864298@qq.com>
2024-06-06 12:11:32 +0800

    提交

---
+++ test/src/algorithm/sort/QuickSort.java
@@ -0,0 +1,20 @@
+package algorithm.sort;
+
+/**
+ * @Author: 皮皮
+ * @Date: 2024/6/6 10:55
+ * @Description:
+ */
+
+
+public class QuickSort {
+
+
+    public static void main(String[] args) {
+        int a[] = new int[]{7,5,8,1,2,4};
+
+
+
+
+    }
+}
